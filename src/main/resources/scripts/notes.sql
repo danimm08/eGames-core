a81c35df6f265e68f579bff7df63199b9442f760
Daniel Moreno Márquez <danimoreno.m08@gmail.com>
2017-04-04 11:31:41 +0200

    Updated SQL scripts to clean them from old data

--- src/main/resources/scripts/notes.sql
+++ src/main/resources/scripts/notes.sql
@@ -1,5 +1,5 @@
-#Notes
-INSERT INTO note (date, text, exchange_id, user_id) VALUES ("2017-01-05 20:15","Creo que el intercambi será positivo para ambos",1,1);
-INSERT INTO note (date, text, exchange_id, user_id) VALUES ("2017-01-05 20:30","Creo que mi juego es mejor, así que deberías darme dos juegos",1,5);
-INSERT INTO note (date, text, exchange_id, user_id) VALUES ("2017-01-06 12:30","En absoluto, si quieres intercambiamos así.",1,1);
-INSERT INTO note (date, text, exchange_id, user_id) VALUES ("2017-01-07 17:30","Bueno, vale",1,5);
+#Note
+INSERT INTO eGames.note (version, date, text, exchange_id, user_id) VALUES (0, '2017-01-05 20:15:00', 'Creo que el intercambi será positivo para ambos', 1, 1);
+INSERT INTO eGames.note (version, date, text, exchange_id, user_id) VALUES (0, '2017-01-05 20:30:00', 'Creo que mi juego es mejor, así que deberías darme dos juegos', 1, 5);
+INSERT INTO eGames.note (version, date, text, exchange_id, user_id) VALUES (0, '2017-01-06 12:30:00', 'En absoluto, si quieres intercambiamos así.', 1, 1);
+INSERT INTO eGames.note (version, date, text, exchange_id, user_id) VALUES (0, '2017-01-07 17:30:00', 'Bueno, vale', 1, 5);
--- src/main/resources/scripts/qualifications.sql
+++ src/main/resources/scripts/qualifications.sql
@@ -1,5 +1,5 @@
-#Qualifications
-INSERT INTO qualification (mark, text, exchange_id, user_id) VALUES (10,"Intercambiamos sin ningún problema. Es un tío de fiar", 1, 1);
-INSERT INTO qualification (mark, text, exchange_id, user_id) VALUES (8,"Bien, pero su pedido llegó un poco más tarde de lo acordado", 1, 5);
-INSERT INTO qualification (mark, text, exchange_id, user_id) VALUES (5,"Sus juegos no estaban en un buen estado", 5, 8);
-INSERT INTO qualification (mark, text, exchange_id, user_id) VALUES (1,"Muy mal, me envió un juego que no leía mi consola, estaba dañado", 5, 1);
+#Qualification 
+INSERT INTO eGames.qualification (version, mark, text, exchange_id, user_id) VALUES (0, 10, 'Intercambiamos sin ningún problema. Es un tío de fiar', 1, 1);
+INSERT INTO eGames.qualification (version, mark, text, exchange_id, user_id) VALUES (0, 8, 'Bien, pero su pedido llegó un poco más tarde de lo acordado', 1, 5);
+INSERT INTO eGames.qualification (version, mark, text, exchange_id, user_id) VALUES (0, 5, 'Sus juegos no estaban en un buen estado', 5, 8);
+INSERT INTO eGames.qualification (version, mark, text, exchange_id, user_id) VALUES (0, 1, 'Muy mal, me envió un juego que no leía mi consola, estaba dañado', 5, 1);
