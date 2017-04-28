df5159c5c9246c82dad9f8fdc43c882b05371928
Daniel Moreno Márquez <danimoreno.m08@gmail.com>
2017-04-28 18:38:28 +0200

    Miscelánea

--- src/main/java/es/eGames/repositories/PersonalGameRepository.java
+++ src/main/java/es/eGames/repositories/PersonalGameRepository.java
@@ -35,13 +35,13 @@ public interface PersonalGameRepository extends JpaRepository<PersonalGame, Inte
     @Query("select pg from PersonalGame pg where pg.exchange.id = ?1 and pg.user.id = ?2")
     List<PersonalGame> findAllPersonalGameByUserAndExchange(int exchangeId, int userId);
 
-    @Query("select pg from PersonalGame pg where pg.user.userAccount.username not like ?1")
+    @Query("select pg from PersonalGame pg where pg.exchange is null and pg.user.userAccount.username not like ?1")
     List<PersonalGame> findAllExceptOfPrincipal(String username);
 
     @Query("select pg from PersonalGame pg where pg.game.id = ?1 and pg.user.id <> ?2 and pg.exchange is null and pg.user = ANY (select fw from User u join u.followees fw where u.id = ?2 )")
     List<PersonalGame> findPersonalGamesOfFolloweesByGameId(int gameId, int principalId);
 
-    @Query("select pg from PersonalGame pg where pg.user = ANY (select fw from User u join u.followees fw where u.id = ?1 )")
+    @Query("select pg from PersonalGame pg where pg.exchange is null and pg.user = ANY (select fw from User u join u.followees fw where u.id = ?1 )")
     List<PersonalGame> findPersonalGamesOfFollowees(int principalId);
 
     @Query("select g from PersonalGame g where g.description like %?1% or g.game.title like %?1%")
